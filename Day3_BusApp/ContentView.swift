d75a228bcf26fc5f166102ab82a5cd19135383b1
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2021-08-04 10:05:56 -0500

    Initial Commit

---
+++ Day3_BusApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day3_BusApp
+//
+//  Created by Jeremy Fleshman on 8/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day3_BusApp/Day3_BusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day3_BusAppApp.swift
+//  Day3_BusApp
+//
+//  Created by Jeremy Fleshman on 8/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Day3_BusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
